9a12d1baa3e5328c07fd19c8746108fe0e902b27
Emil <110348526+emil-apps@users.noreply.github.com>
2025-03-13 15:07:11 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,68 @@
-var tMain = document.getElementById("t-main");
-var tAdvanced = document.getElementById("t-advanced");
-var tSecurity = document.getElementById("t-security");
-var tBoot = document.getElementById("t-boot");
-var tExit = document.getElementById("t-exit");
+let tabs = [
+    {
+        button: "tb-info",
+        tab: "t-info"
+    },
+    {
+        button: "tb-system",
+        tab: "t-system"
+    },
+    {
+        button: "tb-advanced",
+        tab: "t-advanced"
+    },
+    {
+        button: "tb-security",
+        tab: "t-security"
+    },
+    {
+        button: "tb-boot",
+        tab: "t-boot"
+    },
+    {
+        button: "tb-exit",
+        tab: "t-exit"
+    },
+];
 
-let activeTab = tMain;
-let activeSetting = null;
+let activeTab = 0;
 
-document.addEventListener("keyup", (e)=>{
-    if(e.key.toLowerCase() == "escape") window.close()
+document.addEventListener("keydown", (e)=>{
+    if(e.key.toLowerCase() != "arrowright" &&
+        e.key.toLowerCase() != "arrowleft") return;
+
+    if(e.key.toLowerCase() == "arrowright")
+    {
+        if(activeTab < tabs.length-1) activeTab++;
+        else activeTab = 0;
+    }
+    else
+    {
+        if(activeTab > 1) activeTab--;
+        else activeTab = tabs.length-1;
+    }
+
+    for(let i=0;i<tabs.length;i++)
+    {
+        document.getElementById(tabs[i].button).classList.remove("active");
+        document.getElementById(tabs[i].tab).classList.add("hidden");
+    }
+
+    document.getElementById(tabs[activeTab].button).classList.add("active");
+    document.getElementById(tabs[activeTab].tab).classList.remove("hidden");
 });
+
+// github icon?
+var urlParams = new URLSearchParams(window.location.search);
+if(urlParams.has("github_icon"))
+{
+    if(urlParams.get("github_icon") == "no") document.body.removeChild(document.getElementById("github-btn"));
+}
+
+// General
+setInterval(() => {
+    var date = new Date();
+
+    document.getElementById("sys-time").innerHTML = "[" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "]";
+    document.getElementById("sys-date").innerHTML = "[" + date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear() + "]"; 
+}, 100);
